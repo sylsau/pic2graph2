968fa4e8a49b9e9ab7c269a47bbfd2924d69d718
Sylvain S <mail@sylsau.com>
2022-04-29 20:35:20 +0200

    adding density and more command-line options

--- pic2graph.sh
+++ pic2graph.sh
@@ -32,21 +32,40 @@
 # We don't have complete option coverage on eqn because this is primarily
 # intended as a pic translator; we can live with eqn defaults. 
 #
-groffpic_opts=""
+groff_opts=""
 convert_opts=""
-convert_trim_arg="-trim"
+convert_trim="-trim"
+convert_density="-density 600"
 format="png"
 eqndelim='$$'
 
 while [ "$1" ]
 do
     case $1 in
-    -unsafe)
-	groffpic_opts="-U";;
-    -format)
+    -U | --unsafe)
+	groff_opts="$groff_opts -U";;
+    -f | --format)
 	format=$2
 	shift;;
-    -eqn)
+    -q | --quality)
+	if [ $2 -eq 1 ]; then
+		convert_density="-density 300"
+	elif [ $2 -eq 2 ]; then
+		convert_density="-density 600"
+	elif [ $2 -eq 3 ]; then
+		convert_density="-density 900"
+	else
+		convert_density="-density $2"
+	fi
+	shift;;
+    -b)
+	convert_opts="$convert_opts -background white -flatten";;
+    -k)
+	groff_opts="$groff_opts -k";;
+    -K)
+	groff_opts="$groff_opts -K $2"
+	shift;;
+    --eqn)
 	eqndelim=$2
 	shift;;
     -v | --version)
@@ -61,8 +80,7 @@ do
     shift
 done
 
-if [ "$eqndelim" ]
-then
+if [ "$eqndelim" ] ; then
     eqndelim="delim $eqndelim"
 fi
 
@@ -100,23 +118,21 @@ fi
 # See if the installed version of convert(1) is new enough to support the -trim
 # option.  Versions that didn't were described as "old" as early as 2008.
 is_convert_recent=`convert -help | grep -e -trim`
-if test -z "$is_convert_recent"
+if [ -z "$is_convert_recent" ]
 then
     echo "$0: warning: falling back to old '-crop 0x0' trim method" >&2
-    convert_trim_arg="-crop 0x0"
+    convert_trim="-crop 0x0"
 fi
 
 trap 'exit_status=$?; rm -rf "$tmp" && exit $exit_status' EXIT INT TERM
 
 # Here goes:
-# 1. Wrap the input in dummy .PS/PE macros (and add possibly null .EQ/.EN)
+# 1. Wrap stdin in dummy .PS/PE macros (and add possibly null .EQ/.EN)
 # 2. Process through eqn and pic to emit troff markup.
 # 3. Process through groff to emit Postscript.
 # 4. Use convert(1) to crop the PostScript and turn it into a bitmap.
 (echo ".EQ"; echo $eqndelim; echo ".EN"; echo ".PS"; cat; echo ".PE") | \
-    groff -e -p $groffpic_opts -Tps -P-pletter > "$tmp"/pic2graph.ps \
-    && convert $convert_trim_arg $convert_opts "$tmp"/pic2graph.ps \
-       "$tmp"/pic2graph.$format \
-    && cat "$tmp"/pic2graph.$format
+	groff -e -p $groff_opts -Tps -P-pletter | \
+	convert $convert_density - $convert_opts $convert_trim $format:-
 
 # End
